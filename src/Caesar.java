7e3e66febdb30f7b310a57359057c97635823ef9
hendrik <hetosche@freenet.de>
2024-11-12 13:21:53 +0100

    caeser

--- src/Caesar.java
+++ src/Caesar.java
@@ -13,11 +13,14 @@ public void verschluessen()
     {
         for(int i = 0; i< Kt.length(); i++ )
         {
-          gt.charAt(i) = (char)(Kt.charAt(i)+s);
+          //gt.charAt(i) = (char)(Kt.charAt(i)+s);
 
+            gt = gt+(char)(Kt.charAt(i)+s);
 
         }
     }
+
+
     public  void entschluessln()
     {
         for(int i = 0; i< Kt.length(); i++ )
@@ -25,17 +28,28 @@ public void verschluessen()
             kt.charAt(i) = (char)(gt.charAt(i)-s);
         }
     }
-    private char getChar() {}
+    private char getChar(int pWert) {
+        return (char)pWert;
+    }
     private int getAscii(char pWert) {
     return (int) pWert;
     }
-    public String getGt() {}
-    public void setGt(String pgt) {}
-    public String getKt(){}
-        public void setKt(String pgt){}
+    public String getGt() {
+    return gt;
+    }
+    public void setGt(String pgt) {
+    String pGt = gt;
+    }
+    public String getKt(){
+   return kt;
+    }
+        public void setKt(String pkt){
+    pkt= kt;
+        }
 
     public int getS() {
         return s;
     }
+
 
 }
