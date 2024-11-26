e6d799359a4891eaef7effbe7bfb26a2b6eb1a53
hendrik <hetosche@freenet.de>
2024-11-26 12:13:50 +0100

    caeser

--- src/Caesar.java
+++ src/Caesar.java
@@ -15,14 +15,19 @@ public Caesar(){
 
 public void verschluessen()
     {
-        System.out.println("ich funktuniere");
+       // System.out.println("ich funktuniere");
         for (int i = 0; i< Kt.length(); i++ ) {
             int h =  getAscii(Kt.charAt(i));
             if (h+s>91)
             {h=h-26;}
             gt= gt + this.getChar(h+s);
            //System.out.println(gt);
-        }//die for schleife geht
+        }//die for schleife geht die Länge des Strings durch. Mithilfe der Methode get Ascii wird
+        // der Ascii code von den Buchstaben vom zu Verschlüsselnden Text. Dieser wird dann mit dem Schlüssel
+        // adiert und wird zurück in einen Buchstaben umgeändert.
+        // Sollte der Ascii wert vom Schlüssel + Verschlüsselnden Text außerhalb des Lateinischen Alphabetes
+        // sein, wird durch die If schleife wieder am Anfang des alphabets angefangen.
+        //
 
     }
 
@@ -33,9 +38,10 @@ public void verschluessen()
 
             int m =  this.getAscii(gt.charAt(i));
             if (m-s<65)
-            {m=m-26;}
+            {m=m+26;}
             Kt = Kt + this.getChar(m-s);
            // System.out.println(Kt);
+            // 
         }
     }
 
