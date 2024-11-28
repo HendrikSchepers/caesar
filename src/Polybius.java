1627696d3758f5ba54d97c5bc66f9b04838ac85d
hendrik <hetosche@freenet.de>
2024-11-28 10:02:36 +0100

    caeser

--- src/Polybius.java
+++ src/Polybius.java
@@ -1,38 +1,37 @@
 public class Polybius extends Kryptomat{
-    public int alphabetQuadrat[][] = new int[7][7];
+    public String alphabetQuadrat[][] = new String[7][7];
 
 
 
     public void Polybius(){
         kt = "";
         gt= "";
-        alphabetQuadrat [0][0]= 0;
+        alphabetQuadrat [0][0]= "0";
     }
     public void bfms(){
         int q = 1;
         int z = 65;
-       int i =1;
-       while (z<91)
+        int i =1;
+        while (z<91)
         {
             if(z == 74)
             { z++;}
-            alphabetQuadrat[q][i] = z;
+            alphabetQuadrat[q][i] =  ""+ this.getChar(z);
             System.out.println(q + "q");
             System.out.println(alphabetQuadrat[q][i]);
-
                     if (i==6)
             {
                 q++;
                 i = 0;
             }
-
             z++;
-
             i++;
         }
 
     }
-    public void verschluesseln(){};
+    public void verschluesseln(){
+
+    };
 
 
     public   void entschluessln(){};
